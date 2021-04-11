b50f7d05956ee9800f2019fb8ec8363e7f878971
mepo <samuray300094@gmail.com>
2021-04-12 02:56:07 +0300

    final challenge Challenge: Modify the DiceRoller app to have two dice. When the user taps the Roll button, each dice should have a value independent of the other

--- app/src/main/java/com/lmepol/diceroll/MainActivity.kt
+++ app/src/main/java/com/lmepol/diceroll/MainActivity.kt
@@ -7,22 +7,29 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var diceImageView: ImageView
+    private lateinit var diceImageViewFirst: ImageView
+    private lateinit var diceImageViewSecond: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImageView = findViewById(R.id.dice_image)
+        diceImageViewFirst = findViewById(R.id.dice_image_1)
+        diceImageViewSecond = findViewById(R.id.dice_image_2)
         findViewById<Button>(R.id.roll_btn).setOnClickListener {
             roll()
         }
-        findViewById<Button>(R.id.count_up_btn).setOnClickListener {
-            countUp()
-        }
     }
+
     private fun roll() {
-        val rand_val = (1..6).random()
-        val imageRecource = when(rand_val){
+        val randValueFirst = (1..6).random()
+        val randValueSecond = (1..6).random()
+        diceImageViewFirst.setImageResource(getRandomDiceImage(randValueFirst))
+        diceImageViewSecond.setImageResource(getRandomDiceImage(randValueSecond))
+        Toast.makeText(this, "$randValueFirst $randValueSecond", Toast.LENGTH_LONG).show()
+    }
+
+    private fun getRandomDiceImage(i:Int): Int {
+        return when (i) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,19 +38,6 @@ class MainActivity : AppCompatActivity() {
             6 -> R.drawable.dice_6
             else -> R.drawable.dice_1
         }
-        diceImageView.setImageResource(imageRecource)
-        Toast.makeText(this, "$rand_val", Toast.LENGTH_LONG).show()
     }
-    private fun countUp(){
-//        val textView = findViewById<TextView>(R.id.result_text)
-//        val currentValue = try {
-//            textView.text.toString().toInt()
-//        }
-//        catch (e: NumberFormatException){
-//            0
-//        }
-//        if (currentValue<6){
-//            textView.text = (currentValue+1).toString()
-//        }
-    }
+
 }
