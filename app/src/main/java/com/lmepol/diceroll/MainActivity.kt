f5d856bc6860c0e24706808593a30c5c95a8beac
mepo <samuray300094@gmail.com>
2021-04-11 23:23:54 +0300

    add image of dice

--- app/src/main/java/com/lmepol/diceroll/MainActivity.kt
+++ app/src/main/java/com/lmepol/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lmepol.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.lang.NumberFormatException
@@ -20,19 +21,28 @@ class MainActivity : AppCompatActivity() {
     }
     private fun roll() {
         val rand_val = (1..6).random()
-        findViewById<TextView>(R.id.result_text).text = rand_val.toString()
+        val imageRecource = when(rand_val){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+        findViewById<ImageView>(R.id.dice_image).setImageResource(imageRecource)
         Toast.makeText(this, "$rand_val", Toast.LENGTH_LONG).show()
     }
     private fun countUp(){
-        val textView = findViewById<TextView>(R.id.result_text)
-        val currentValue = try {
-            textView.text.toString().toInt()
-        }
-        catch (e: NumberFormatException){
-            0
-        }
-        if (currentValue<6){
-            textView.text = (currentValue+1).toString()
-        }
+//        val textView = findViewById<TextView>(R.id.result_text)
+//        val currentValue = try {
+//            textView.text.toString().toInt()
+//        }
+//        catch (e: NumberFormatException){
+//            0
+//        }
+//        if (currentValue<6){
+//            textView.text = (currentValue+1).toString()
+//        }
     }
 }
