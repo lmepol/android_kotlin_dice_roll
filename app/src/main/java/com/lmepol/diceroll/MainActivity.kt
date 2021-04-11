da30211f3972af48aaa27bc5b701757080e3a009
mepo <samuray300094@gmail.com>
2021-04-11 21:30:05 +0300

    codding challenge complete

--- app/src/main/java/com/lmepol/diceroll/MainActivity.kt
+++ app/src/main/java/com/lmepol/diceroll/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +14,26 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.roll_btn).setOnClickListener{
             roll()
         }
+        findViewById<Button>(R.id.count_up_btn).setOnClickListener{
+            countUp()
+        }
         roll()
     }
     private fun roll() {
         val rand_val = (1..6).random()
         findViewById<TextView>(R.id.result_text).text = rand_val.toString()
         Toast.makeText(this, "$rand_val", Toast.LENGTH_LONG).show()
+    }
+    private fun countUp(){
+        val textView = findViewById<TextView>(R.id.result_text)
+        val currentValue = try {
+            textView.text.toString().toInt()
+        }
+        catch (e: NumberFormatException){
+            0
+        }
+        if (currentValue<6){
+            textView.text = (currentValue+1).toString()
+        }
     }
 }
